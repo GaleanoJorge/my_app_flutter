6d715a6356e4973a7c562ee1f1484d48ad74ece9
GaleanoJorge <juandavidrianoperez@gmail.com>
2021-08-30 18:07:45 -0500

    uso de padding y marging

--- lib/main.dart
+++ lib/main.dart
@@ -11,69 +11,14 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Uso del AppBar'),
         ),
-        body: SizedBox(
-          child: Column(
-            children: [
-              Row(
-                children: <Widget>[
-                  Expanded(child: Text('Activar sonido')),
-                  Icon(
-                    Icons.android,
-                    size: 60,
-                  ),
-                  Switch(
-                    value: true,
-                    onChanged: (value) {},
-                  )
-                ],
-              ),
-              Divider(),
-              Row(
-                children: <Widget>[
-                  Expanded(child: Text('Habilitar el uso de la cámara')),
-                  Icon(
-                    Icons.android,
-                    size: 60,
-                  ),
-                  Switch(
-                    value: true,
-                    onChanged: (value) {},
-                  )
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: <Widget>[
-                  Container(
-                    height: 50,
-                    width: 50,
-                    color: Colors.black,
-                  ),
-                  Container(
-                    height: 50,
-                    width: 50,
-                    color: Colors.orange,
-                  ),
-                  Container(
-                    height: 100,
-                    width: 50,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    height: 50,
-                    width: 50,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    height: 50,
-                    width: 50,
-                    color: Colors.black,
-                  ),
-                ],
-              ),
-            ],
-          ),
+        body: Row(
+          children: <Widget>[
+            Text('Hola'),
+            SizedBox(
+              width: 50,
+            ),
+            Text('Que más!')
+          ],
         ),
       ),
     );
