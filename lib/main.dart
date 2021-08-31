3fe310ad5b1b3bedfb158360eeb031136f52ed0e
GaleanoJorge <juandavidrianoperez@gmail.com>
2021-08-31 17:05:37 -0500

    uso de funtes externas

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      theme: ThemeData(fontFamily: 'Pacifico'),
       home: MyHomePage(),
     );
   }
@@ -19,15 +20,20 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Assets'),
+        title: Text('Uso de Fonts'),
       ),
       body: ListView(
         children: <Widget>[
-          Image.asset('assets/IMG_1.jpg'),
-          Image.asset('assets/IMG_2.jpg'),
-          Image.asset('assets/IMG_3.jpg'),
-          Image.asset('assets/IMG_4.jpg'),
-          Image.asset('assets/IMG_5.jpg'),
+          Text(
+            '''Lorem ipsum dolor sit amet consectetur adipiscing elit nulla at aenean tempus, id porttitor tincidunt felis dignissim tortor vivamus sodales odio semper. Turpis vivamus dictumst proin nascetur augue consequat convallis sociosqu in, commodo conubia phasellus ornare malesuada nisi tellus penatibus, non platea tortor aenean interdum sapien habitasse eu. Viverra vivamus bibendum urna maecenas cum montes fermentum luctus suscipit proin hac, curabitur facilisis augue penatibus fames mus massa sodales metus taciti, porta ullamcorper nulla nec mattis eleifend laoreet dictum nibh inceptos.
+
+Lectus torquent tellus velit aliquet eleifend vel iaculis integer natoque auctor tristique, pretium odio conubia ultrices faucibus inceptos nunc facilisis mollis per. Porta magna dapibus ligula in dictumst vulputate tellus varius proin ut aliquam dignissim ultricies, neque velit dui commodo risus sodales tincidunt leo mauris condimentum natoque. Aenean nullam integer porttitor libero magnis quis, rutrum eros phasellus potenti nam convallis auctor, curae inceptos viverra vestibulum himenaeos.''',
+            style: TextStyle(fontFamily: 'IndieFlower'),
+          ),
+          Text('''Lorem ipsum dolor sit amet consectetur adipiscing elit nulla at aenean tempus, id porttitor tincidunt felis dignissim tortor vivamus sodales odio semper. Turpis vivamus dictumst proin nascetur augue consequat convallis sociosqu in, commodo conubia phasellus ornare malesuada nisi tellus penatibus, non platea tortor aenean interdum sapien habitasse eu. Viverra vivamus bibendum urna maecenas cum montes fermentum luctus suscipit proin hac, curabitur facilisis augue penatibus fames mus massa sodales metus taciti, porta ullamcorper nulla nec mattis eleifend laoreet dictum nibh inceptos.
+
+Lectus torquent tellus velit aliquet eleifend vel iaculis integer natoque auctor tristique, pretium odio conubia ultrices faucibus inceptos nunc facilisis mollis per. Porta magna dapibus ligula in dictumst vulputate tellus varius proin ut aliquam dignissim ultricies, neque velit dui commodo risus sodales tincidunt leo mauris condimentum natoque. Aenean nullam integer porttitor libero magnis quis, rutrum eros phasellus potenti nam convallis auctor, curae inceptos viverra vestibulum himenaeos.''',
+            style: TextStyle(fontFamily: 'Pacifico'),),
         ],
       ),
     );
