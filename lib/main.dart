6024c15d7c125696a4330114b37bed654bed2866
GaleanoJorge <juandavidrianoperez@gmail.com>
2021-08-31 15:16:08 -0500

    list view con textos

--- lib/main.dart
+++ lib/main.dart
@@ -7,19 +7,53 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  List<String> names = [
+    'Jorge',
+    'Nathaly',
+    'Jim',
+    'Andres',
+    'Stefanny',
+    'Jorge',
+    'Nathaly',
+    'Jim',
+    'Andres',
+    'Stefanny',
+    'Jorge',
+    'Nathaly',
+    'Jim',
+    'Andres',
+    'Stefanny'
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
         appBar: AppBar(
           title: Text('Uso del AppBar'),
         ),
-        body: Center(
-          child: Container(
-            // color: Colors.blue[900],
-            // color: Color(0xff1DD1FE).withOpacity(0.5), // para definir un color mediante HEX se debe iniciar con 0xff (lo cual es el valor de opacidad en HEX) {HEX (sin el #)}
-            color: Color.fromRGBO(29, 209, 254, 0.8),
-            height: 300,
-            width: 300,
-          ),
-        )
+        body: ListView.builder(
+            itemCount: names.length,
+            itemBuilder: (BuildContext context, int index) {
+              final name = names[index];
+              return Card(
+                child: ListTile(
+                  title: Text(name),
+                  leading: Icon(Icons.person),
+                  onTap: (){
+                    print(name);
+                  },
+                ),
+              );
+            }),
       ),
     );
   }
