75cf2f434742ff9628edfa42a703b0ddaf1212d8
GaleanoJorge <juandavidrianoperez@gmail.com>
2021-08-30 20:23:43 -0500

    colores

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,15 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Uso del AppBar'),
         ),
-        body: Row(
-          children: <Widget>[
-            Text('Hola'),
-            SizedBox(
-              width: 50,
-            ),
-            Text('Que más!')
-          ],
-        ),
+        body: Center(
+          child: Container(
+            // color: Colors.blue[900],
+            // color: Color(0xff1DD1FE).withOpacity(0.5), // para definir un color mediante HEX se debe iniciar con 0xff (lo cual es el valor de opacidad en HEX) {HEX (sin el #)}
+            color: Color.fromRGBO(29, 209, 254, 0.8),
+            height: 300,
+            width: 300,
+          ),
+        )
       ),
     );
   }
